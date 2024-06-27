b616d7bffc257873de31c958ec893b3cf513db5a
Ashmeet Singh <ashmeetsmonga@gmail.com>
2024-06-27 17:28:47 +0530

    func parameter added

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,6 +1,17 @@
 package main
+
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var num int = 193
+	var denom int = 4
+
+	var result, remainder int = division(num, denom)
+	fmt.Printf("The result when %v is divided by %v is %v and remainder is %v", num, denom, result, remainder)
+}
+
+func division(num int, denom int) (int, int) {
+	var result int = num / denom
+	var remainder int = num % denom
+	return result, remainder
 }
