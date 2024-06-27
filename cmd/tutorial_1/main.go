2e4baf6982440c91e6ab0c7b58cb479390b9d008
Ashmeet Singh <ashmeetsmonga@gmail.com>
2024-06-27 17:37:07 +0530

    error handling added

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,17 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	var num int = 193
-	var denom int = 4
+	var denom int = 0
 
-	var result, remainder int = division(num, denom)
-	fmt.Printf("The result when %v is divided by %v is %v and remainder is %v", num, denom, result, remainder)
+	var result, remainder, err = division(num, denom)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("The result when %v is divided by %v is %v and remainder is %v", num, denom, result, remainder)
+	}
+
 }
 
-func division(num int, denom int) (int, int) {
+func division(num int, denom int) (int, int, error) {
+	var err error
+	if denom == 0 {
+		err = errors.New("cannot divide by 0")
+		return 0, 0, err
+	}
 	var result int = num / denom
 	var remainder int = num % denom
-	return result, remainder
+	return result, remainder, err
 }
